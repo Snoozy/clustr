006934f22670f905564e7bae8d5efb15e5cb1903
Daniel <danielli803@gmail.com>
2015-10-17 22:09:42 -0700

    Database point

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,32 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.Point
+import play.api.libs.json.{JsValue, Json}
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+    def ping = Action { implicit request =>
+      val longitude = request.getQueryString("longitude")
+      val latitude = request.getQueryString("latitude")
+      val deviceToken = request.getQueryString("device_token")
+      if (longitude.isDefined && latitude.isDefined && deviceToken.isDefined) {
+          Point.create(latitude.get.toDouble, longitude.get.toDouble, deviceToken.get)
+          Ok(Json.obj("status" -> "success"))
+      } else {
+          BadRequest("status" -> "error")
+      }
+    }
+
+    private val widthFactor = 100
+    private val heightFactor = 100
+
+    def getMap = Action { implicit request =>
+        val cornerTLX = request.getQueryString("corner_tl_x")
+        val cornerTLY = request.getQueryString("corner_tl_y")
+        val cornerBRX = request.getQueryString("corner_br_x")
+        val cornerBRY = request.getQueryString("corner_br_y")
+        Ok("")
+    }
 
 }
--- app/models/Point.scala
+++ app/models/Point.scala
@@ -8,7 +8,43 @@ import play.api.db._
 import play.api.libs.json._
 
 case class Point (
+    pointId: Int,
     latitude: Double,
     longitude: Double,
     deviceToken: String
 )
+
+object Point {
+
+    private val pointParser: RowParser[Point] = {
+        get[Option[Int]]("point_id") ~
+            get[Double]("latitude") ~
+            get[Double]("longitude") ~
+            get[String]("device_token") map {
+            case pointId ~ latitude ~ longitude ~ deviceToken =>
+                Point(pointId.get, latitude, longitude, deviceToken)
+        }
+    }
+
+    def create(latitude: Double, longitude: Double, deviceToken: String) = {
+        DB.withConnection { implicit connection =>
+            SQL("INSERT INTO point (latitude, longitude, device_token) VALUES ({lat}, {long}, {device}")
+                .on('lat -> latitude, 'long -> longitude, 'device -> deviceToken).executeUpdate()
+        }
+    }
+
+    def byDeviceToken(deviceToken: String) = {
+        DB.withConnection { implicit connection =>
+            SQL("SELECT * FROM point WHERE device_token = {device} LIMIT 1")
+                .on('device -> deviceToken).as(pointParser.singleOpt)
+        }
+    }
+
+    def byCoordinates(latitude: Double, longitude: Double, offset: Int) = {
+        DB.withConnection { implicit connection =>
+            SQL("SELECT * FROM point WHERE (latitude > {lat} - {offset} AND latitude < {lat} + {offset}) AND (longitude > {long} - {offset} AMD longitude < {long} + {offset})")
+                .on('lat -> latitude, 'long -> longitude, 'offset -> offset).as(pointParser *)
+        }
+    }
+
+}
